87e0fc5dbf2cc9cdaf7cf02395ce6585bb657cf7
reckonerv <disposables@mailinator.com>
2017-03-09 21:56:08 +0900

    Fix.

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/amazon/fsm/HelloTransition.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/amazon/fsm/HelloTransition.java
@@ -1,6 +1,8 @@
 package com.example.bot.spring.amazon.fsm;
 
 import com.example.bot.spring.amazon.bot.Conversation;
+import com.example.bot.spring.amazon.model.BotActionResponse;
+import com.example.bot.spring.amazon.model.ResponseType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -21,6 +23,10 @@ public class HelloTransition implements Transition<Conversation> {
     @Override
     public StateActionPair<Conversation> getStateActionPair(Conversation c, String event, Object... args) throws RetryException {
         if (c.getLastInput().getText().equals("I want to buy")) {
+            c.addResponse(BotActionResponse.builder()
+                    .responseText("What do you want to buy?")
+                    .responseType(ResponseType.TEXT)
+                    .build());
             return goToBuy;
         } else {
             return goToHello;
